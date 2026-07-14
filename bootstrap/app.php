bbce85a582d5cb6bd1fa9e5e6d594a15017089d1
Aguilar & Arce <admin@aguilar-arce.com>
2026-07-13 23:18:31 -0500

    fix: configure trusted proxies for nginx reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: ['172.16.0.0/12', '10.0.0.0/8']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
